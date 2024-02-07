d4bf8ede7c03336452d1f9dd30cda145874a4135
marmionsteven8 <marmionsteven8@gmail.com>
2024-02-07 08:09:32 +0100

    Ajout de la base pour récupérer les informations sur profil

--- App/Views/Profil/UserProfil.php
+++ App/Views/Profil/UserProfil.php
@@ -1,15 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
+
+require_once __DIR__ . '/../Autoloader/autoloader.php';
+
+use \App\Autoloader\Autoloader;
+use App\Models\EntityOperations\CrudUser;
+use Database\DatabaseConnection\ConnexionBDD;
+
+Autoloader::register();
+
 session_start();
 
 // Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['user'])) {
+if (!isset($_SESSION['id'])) {
     header("Location: /login.php"); // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
     exit();
 }
 
-$user = $_SESSION['user'];
+$idCurrentUser = $_SESSION['id'];
 
+$db = new ConnexionBDD();
+$crudUser = new CrudUser($db::obtenir_connexion());
+
+$userDatas = $crudUser->obtenirUtilisateurParId($idCurrentUser);
+// $userDatas["idU"] = ....
+// $userDatas["pseudo"] = ... 
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +45,7 @@ $user = $_SESSION['user'];
     <h2>Profil Utilisateur</h2>
 
     <div class="profile-info">
-        <p><strong>Pseudo :</strong> <?php echo $user['pseudo']; ?></p>
+        <p><strong>Pseudo :</strong> <?php echo $userDatas['pseudo']; ?></p>
         <p><strong>Email :</strong> <?php echo $user['adresseMail']; ?></p>
         <p><strong>Mot de passe :</strong> ********</p>
 
