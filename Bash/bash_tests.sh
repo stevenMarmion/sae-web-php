1219a524f4b7bd91ea105355d6cb3d91431bd61f
stevenMarmion <marmionsteven7@gmail.com>
2024-02-08 22:15:55 +0100

    Finition des tests sur la connexionBDD PDO sqlite

--- Bash/bash_tests.sh
+++ Bash/bash_tests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Se déplacer dans le répertoire contenant les tests
+# Se déplacer dans le répertoire contenant les tests des modèles
 cd Test/ModelsTest
+
+# Boucler à travers tous les fichiers se terminant par "Test.php"
+for file in *Test.php; do
+    echo "Exécution de $file :"
+    php "$file"
+    echo "----------------------------------------"
+done
+
+# Se déplacer dans le répertoire contenant les tests de la base de données
+cd ../DatabaseTest
 
 # Boucler à travers tous les fichiers se terminant par "Test.php"
 for file in *Test.php; do
