45261439b5ad796768827bffb97b2f07630a2cf2
marmionsteven8 <marmionsteven8@gmail.com>
2024-02-09 15:51:08 +0100

    Résolution du conflit d'ajout d'un album dans les favoris d'un USER pour la visualisation des données

--- App/Views/Admin/Details/PanelDetails.php
+++ App/Views/Admin/Details/PanelDetails.php
@@ -45,7 +45,8 @@ if (isset($_GET['table'])) {
                 foreach ($allFavoris as $favori) {
                     $currentFavori = new Favori($favori["idU"], $favori["idAl"]);
                     $currentAlbum = $crudAlbum->obtenirAlbumParId($currentFavori->getIdAlbum());
-                    $currentUser->ajouterFavori($currentAlbum["id"]);
+                    $currentAlbum = new Album($currentAlbum["id"],$currentAlbum["img"] ?? "",$currentAlbum["dateDeSortie"],$currentAlbum["titre"],[],[],[]);
+                    $currentUser->ajouterFavori($currentAlbum);
                 }
                 $allUsersObject[] = $currentUser;
             }
@@ -94,15 +95,17 @@ if (isset($_GET['table'])) {
 
                 // Récupérer les genres
                 $genres = $crudEtre->obtenirGenresParMusique($album["id"]);
-                if (sizeof($genres) == 0) {
-                    $currentGenre = new Genre(-1, "Inconnu");
-                    $currentAlbum->ajouterGenre($currentGenre);
-                }
-                else {
-                    foreach ($genres as $genre) {
-                        $currentGenre = $crudGenre->obtenirGenreParId($genre["idG"]);
-                        $currentGenre = new Genre($genre["idG"],$currentGenre['nomG']);
+                if (is_array($genres)) {
+                    if (sizeof($genres) == 0) {
+                        $currentGenre = new Genre(-1, "Inconnu");
                         $currentAlbum->ajouterGenre($currentGenre);
+                    }
+                    else {
+                        foreach ($genres as $genre) {
+                            $currentGenre = $crudGenre->obtenirGenreParId($genre["idG"]);
+                            $currentGenre = new Genre($genre["idG"],$currentGenre['nomG']);
+                            $currentAlbum->ajouterGenre($currentGenre);
+                        }
                     }
                 }
 
--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-header('Location: /../App/Views/Auth/UserLogin.php');
-//header('Location: /../App/Views/Admin/PanelAdmin.php');
+//header('Location: /../App/Views/Auth/UserLogin.php');
+header('Location: /../App/Views/Admin/PanelAdmin.php');
 //header('Location: /../App/Views/Home/accueil.php');
 exit();
 
