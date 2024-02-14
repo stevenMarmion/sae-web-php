9d47f65ad614b4039906b967966c9d655485cc61
stevenMarmion <marmionsteven7@gmail.com>
2024-02-14 22:46:03 +0100

    Mise en place de la protection de la route vers le panel admin

--- App/Views/Admin/PanelAdmin.php
+++ App/Views/Admin/PanelAdmin.php
@@ -15,6 +15,8 @@ use \App\Models\EntityOperations\CrudGenre;
 
 Autoloader::register();
 
+session_start();
+
 $db = new ConnexionBDD();
 
 $crudUser = new CrudUser($db::obtenir_connexion());
@@ -27,6 +29,19 @@ $listeAlbums = $crudAlbum->obtenirTousAlbums();
 $listeArtiste = $crudArtiste->obtenirTousArtistes();
 $listeGenres = $crudGenre->obtenirTousGenres();
 
+if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
+    if (intval($_SESSION['id']) == 1) { // est notre admin
+        $routeAccept = true;
+        $estAuth = true;
+    } else {
+        $routeAccept = false;
+        $estAuth = true;
+    }
+} else {
+    $estAuth = false;
+    $routeAccept = false;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -37,45 +52,66 @@ $listeGenres = $crudGenre->obtenirTousGenres();
     <link rel="stylesheet" href="/Public/Css/Admin/panel-admin-style.css">
     <title>Panel - Admin</title>
 </head>
-<body>
-    <div class="navigation-gauche">
-        <img id="logo" src="/Public/Images/logo.png" width="200">
-        <nav>
-            <a id="panel-admin-link" href="Details/PanelDetails.php?table=UTILISATEUR">Gérer les utilisateurs</a>
-            <a id="panel-admin-link" href="Details/PanelDetails.php?table=ALBUMS">Gérer les albums</a>
-            <a id="panel-admin-link" href="Details/PanelDetails.php?table=ARTISTES">Gérer les artistes</a>
-            <a id="panel-admin-link" href="Details/PanelDetails.php?table=GENRES">Gérer les genres</a>
-            <a id="panel-admin-link" href="/App/Views/Profil/UserProfil.php">
-                <img id="icon-retour" src="/Public/Icons/back.png">    
-                Revenir sur la plateforme
-            </a>
-        </nav>
-    </div>
-    <section>
-        <div>
-            <h1>Bienvenue dans le panel admin</h1>
+<?php if ($routeAccept) : ?>
+    <body>
+        <div class="navigation-gauche">
+            <img id="logo" src="/Public/Images/logo.png" width="200">
+            <nav>
+                <a id="panel-admin-link" href="Details/PanelDetails.php?table=UTILISATEUR">Gérer les utilisateurs</a>
+                <a id="panel-admin-link" href="Details/PanelDetails.php?table=ALBUMS">Gérer les albums</a>
+                <a id="panel-admin-link" href="Details/PanelDetails.php?table=ARTISTES">Gérer les artistes</a>
+                <a id="panel-admin-link" href="Details/PanelDetails.php?table=GENRES">Gérer les genres</a>
+                <a id="panel-admin-link" href="/App/Views/Profil/UserProfil.php">
+                    <img id="icon-retour" src="/Public/Icons/back.png">    
+                    Revenir sur la plateforme
+                </a>
+            </nav>
         </div>
-        <section class="info-appli">
-            <div class="info-card">
-                <div class="info-label">Nombre d'utilisateurs :</div>
-                <div class="info"><?= sizeof($listeUsers) ?></div>
+        <section>
+            <div>
+                <h1>Bienvenue dans le panel admin</h1>
             </div>
+            <section class="info-appli">
+                <div class="info-card">
+                    <div class="info-label">Nombre d'utilisateurs :</div>
+                    <div class="info"><?= sizeof($listeUsers) ?></div>
+                </div>
 
-            <div class="info-card">
-                <div class="info-label">Nombre d'albums référencés :</div>
-                <div class="info"><?= sizeof($listeAlbums) ?></div>
-            </div>
+                <div class="info-card">
+                    <div class="info-label">Nombre d'albums référencés :</div>
+                    <div class="info"><?= sizeof($listeAlbums) ?></div>
+                </div>
 
-            <div class="info-card">
-                <div class="info-label">Nombre d'artistes référencés :</div>
-                <div class="info"><?= sizeof($listeArtiste) ?></div>
-            </div>
+                <div class="info-card">
+                    <div class="info-label">Nombre d'artistes référencés :</div>
+                    <div class="info"><?= sizeof($listeArtiste) ?></div>
+                </div>
 
-            <div class="info-card">
-                <div class="info-label">Nombre de genres référencés :</div>
-                <div class="info"><?= sizeof($listeGenres) ?></div>
-            </div>
+                <div class="info-card">
+                    <div class="info-label">Nombre de genres référencés :</div>
+                    <div class="info"><?= sizeof($listeGenres) ?></div>
+                </div>
+            </section>
         </section>
+    </body>
+<?php else : ?>
+    <section class="avertissement">
+        <h1 class="error-avertissement">Erreur 404 - Tu n'as pas accès à cette page malheureusement, reviens en lieu sûr... </h1>
+        <?php if ($estAuth) : ?>
+            <div class="retour">
+                <a href="/App/Views/Home/Accueil.php">
+                    <img id="icon-retour" src="/Public/Icons/back.png">
+                    Revenir en lieu sûr
+                </a>
+            </div>
+        <?php else: ?>
+            <div class="retour">
+                <a href="/App/Views/Auth/UserLogin.php">
+                    <img id="icon-retour" src="/Public/Icons/back.png">
+                    Revenir en lieu sûr
+                </a>
+            </div>
+        <?php endif; ?>
     </section>
-</body>
+<?php endif; ?>
 </html>
